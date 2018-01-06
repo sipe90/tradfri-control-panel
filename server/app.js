b16443ceba0ac9681c86d2cb9f11d169220fccd2
Pasi Härme <pasi.harme@siili.com>
2018-01-06 13:33:51 +0200

    Fixed errors which prevented the app from running

--- server/app.js
+++ server/app.js
@@ -6,7 +6,6 @@ let cookieParser = require('cookie-parser');
 let bodyParser = require('body-parser');
 
 let index = require('./routes/index');
-let users = require('./routes/users');
 
 let app = express();
 
@@ -18,7 +17,6 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/', index);
-app.use('/users', users);
 
 app.use((req, res, next) => {
   res.status = 404;
