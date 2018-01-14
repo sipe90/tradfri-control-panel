7e814fee456f4778e5d855397e25d955694fb232
Pasi Härme <pasi.harme@gmail.com>
2018-01-14 14:52:46 +0200

    Updated jest configuration and fixed App.test.js

--- client/config-overrides.js
+++ client/config-overrides.js
@@ -1,6 +1,15 @@
-const { injectBabelPlugin } = require('react-app-rewired');
+const { injectBabelPlugin } = require('react-app-rewired')
 
-module.exports = (config, env) => {
-    config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config);
-    return config;
-};
+module.exports = {
+    webpack: (config /*, env*/ ) => {
+        config = injectBabelPlugin(['import', { libraryName: 'antd', libraryDirectory: 'es', style: 'css' }], config)
+        return config
+    },
+    jest: (config) => {
+        if (!config.testMatch) {
+            config.testMatch = []
+        }
+        config.testMatch.push('<rootDir>/test/**/?(*.)(spec|test).{js,jsx,mjs}')
+        return config
+    }
+}
