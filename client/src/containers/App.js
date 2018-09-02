ab6f5870fc9e65427840d401204a3aa0de39d67f
Pasi Härme <pasi.harme@gmail.com>
2018-09-02 14:15:55 +0300

    Simplified site layout and made it more responsive

--- client/src/containers/App.js
+++ client/src/containers/App.js
@@ -1,8 +1,7 @@
 import React, { Component } from 'react'
 import { Route, Redirect } from 'react-router'
 import { BrowserRouter as Router } from 'react-router-dom'
-import { Layout, Badge, Avatar } from 'antd'
-import BellIcon from 'mdi-react/BellIcon'
+import { Layout } from 'antd'
 
 import Navigation from 'components/Navigation'
 import Routes from 'components/Routes'
@@ -10,41 +9,17 @@ import './App.css'
 
 import routeDefs from 'routeDefs'
 
-const { Header, Sider, Content, Footer } = Layout
+const { Header, Content, Footer } = Layout
 
 class App extends Component {
 
-    constructor() {
-        super()
-
-        this.state = {
-            collapsed: false
-        }
-    }
-
-    collapse(collapsed) {
-        this.setState({ collapsed: collapsed })
-    }
-
     render() {
         return (
-            <Router>
-                <Layout className="app-root">
-                    <Sider
-                        collapsed={this.state.collapsed}
-                        collapsedWidth={80}
-                        collapsible={true}
-                        onCollapse={this.collapse.bind(this)}
-                    >
-                        <div className="app-title">{this.state.collapsed ? null : 'Trådfri Control Panel'}</div>
-                        <Navigation collapsed={this.state.collapsed} routes={routeDefs} />
-                    </Sider>
-                    <Layout>
+            <div className='app-root'>
+                <Router>
+                    <Layout className="app-content">
                         <Header className="app-header">
-                            <Avatar className="app-header-avatar" size="large">PH</Avatar>
-                            <Badge count={5} className="app-header-badge">
-                                <BellIcon className="app-header-icon" size={28} />
-                            </Badge>
+                            <Navigation routes={routeDefs} />
                         </Header>
                         <Content>
                             <Route exact path='/' render={() => <Redirect to={routeDefs[0].path} />} />
@@ -52,8 +27,8 @@ class App extends Component {
                         </Content>
                         <Footer className="app-footer">Trådfri Control Panel ©2018</Footer>
                     </Layout>
-                </Layout>
-            </Router>
+                </Router>
+            </div>
         )
     }
 }
