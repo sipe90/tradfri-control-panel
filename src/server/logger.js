31c1e30be2a55054da03fd2aaff26a176fcaf8fd
Pasi Härme <pasi.harme@gmail.com>
2018-09-09 16:24:56 +0300

    Updated runtime libraries

--- src/server/logger.js
+++ src/server/logger.js
@@ -1,22 +1,30 @@
 const winston = require('winston')
 const path = require('path')
 
-const { Logger, transports: { Console, File } } = winston
+const { createLogger, format, transports: { Console, File } } = winston
+const { combine, timestamp, printf } = format
 
-const logger = new Logger({
+const stringFormat = printf((info) => `${info.timestamp} [${info.level}] ${info.message}`)
+
+const logger = createLogger({
+    format: combine(
+        timestamp({ format: 'YYYY-MM-DD HH:mm:ss.SSS' }),
+        stringFormat,
+    ),
     transports: [
-        new Console(),
+        new Console({
+            handleExceptions: true
+        }),
         new File({
             name: 'logfile',
-            filename: path.join(__dirname, '../../logs/server.log'),
-            json: false
+            filename: path.join(__dirname, '../../logs/server.log')
         }),
         new File({
             name: 'errorlogfile',
             level: 'error',
             filename: path.join(__dirname, '../../logs/error.log'),
-            json: false
-        }),
+            handleExceptions: true
+        })
     ]
 })
 
