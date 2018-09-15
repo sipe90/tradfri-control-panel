d65447262734749d012a7c04244d84ef2ffb0f2f
Pasi Härme <pasi.harme@gmail.com>
2018-09-15 14:54:26 +0300

    Created a component for initial loading spinner

--- src/client/components/lights/LightList.js
+++ src/client/components/lights/LightList.js
@@ -1,28 +1,27 @@
 import React from 'react'
-import { Spin, Icon, List } from 'antd'
+import { List } from 'antd'
 import PropTypes from 'prop-types'
 import * as R from 'ramda'
 
+import Spinner from 'components/Spinner'
 import LightItem from 'components/lights/LightItem'
 
 import 'components/lights/LightList.css'
 
-const LightList = (props) =>
-    (
-        <Spin spinning={props.initialDataLoading} style={{ marginTop: '240px' }} indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}>
-            <List itemLayout='vertical'
-                dataSource={R.values(props.lights)}
-                renderItem={(item) => renderItem(item, props)} />
-        </Spin>
-    )
+const LightList = (props) => (
+    <Spinner spinning={props.initialDataLoading}>
+        <List itemLayout='vertical'
+            dataSource={R.values(props.lights)}
+            renderItem={(item) => renderItem(item, props)} />
+    </Spinner>
+)
 
-const renderItem = (light, { lightStateChanged, updateLight }) =>
-    (
-        <LightItem key={light.id}
-            light={light}
-            lightStateChanged={lightStateChanged}
-            updateLight={updateLight} />
-    )
+const renderItem = (light, { lightStateChanged, updateLight }) => (
+    <LightItem key={light.id}
+        light={light}
+        lightStateChanged={lightStateChanged}
+        updateLight={updateLight} />
+)
 
 LightList.propTypes = {
     lights: PropTypes.object.isRequired,
--- src/client/components/sensors/SensorList.js
+++ src/client/components/sensors/SensorList.js
@@ -1,27 +1,27 @@
 import React from 'react'
-import { Spin, Icon, List } from 'antd'
+import { List } from 'antd'
 import PropTypes from 'prop-types'
 import * as R from 'ramda'
 
+import Spinner from 'components/Spinner'
 import SensorItem from 'components/sensors/SensorItem'
 
 import 'components/sensors/SensorList.css'
 
 const SensorList = (props) => (
-    <Spin spinning={props.initialDataLoading} style={{ marginTop: '240px' }} indicator={<Icon type="loading" style={{ fontSize: 24 }} spin />}>
+    <Spinner spinning={props.initialDataLoading}>
         <List itemLayout='vertical'
             dataSource={R.values(props.sensors)}
             renderItem={(item) => renderItem(item, props)} />
-    </Spin>
+    </Spinner>
 )
 
-const renderItem = (sensor, { sensorStateChanged, updateSensor }) =>
-    (
-        <SensorItem key={sensor.id}
-            sensor={sensor}
-            sensorStateChanged={sensorStateChanged}
-            updateSensor={updateSensor} />
-    )
+const renderItem = (sensor, { sensorStateChanged, updateSensor }) => (
+    <SensorItem key={sensor.id}
+        sensor={sensor}
+        sensorStateChanged={sensorStateChanged}
+        updateSensor={updateSensor} />
+)
 
 SensorList.propTypes = {
     sensors: PropTypes.object.isRequired,
