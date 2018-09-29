13df114ec5b25b743d59bcc8543bc1a98d630f5b
Pasi Härme <pasi.harme@gmail.com>
2018-09-29 23:21:00 +0300

    WIP: Light groups

--- src/client/components/lights/LightGroups.jsx
+++ src/client/components/lights/LightGroups.jsx
@@ -1,24 +1,37 @@
 import React from 'react'
 import PropTypes from 'prop-types'
-import { Collapse } from 'antd'
+import { Card, Collapse, Switch, Slider } from 'antd'
 import * as R from 'ramda'
 
 import LightList from 'components/lights/LightList'
 
 const { Panel } = Collapse
 
+const percentFormatter = (v) => `${v}%`
+
 const LightGroups = (props) => (
-    <Collapse bordered={false}>
-        {R.values(props.groups).map((group, idx) =>
-            <Panel key={idx} header={group.name}>
-                <LightList
-                    lights={lightsForGroup(group, props.lights)}
-                    initialDataLoading={props.initialDataLoading}
-                    lightStateChanged={props.lightStateChanged}
-                    updateLight={props.updateLight}
-                />
-            </Panel>)}
-    </Collapse>
+
+    R.values(props.groups).map((group, idx) =>
+        <Card key={idx} style={{ marginBottom: 8 }} bodyStyle={{ padding: 12 }}>
+            <div style={{ marginBottom: 8, color: 'rgba(0, 0, 0, 0.85)', fontWeight: 450 }}>{group.name}</div>
+            <LightGroupControls group={group} />
+            <Collapse bordered={true} style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
+                <Panel header='Lights'>
+                    <LightList
+                        lights={lightsForGroup(group, props.lights)}
+                        initialDataLoading={props.initialDataLoading}
+                        lightStateChanged={props.lightStateChanged}
+                        updateLight={props.updateLight}
+                    />
+                </Panel>
+            </Collapse>
+        </Card>)
+)
+
+const LightGroupControls = (props) => (
+    <div>
+
+    </div>
 )
 
 const lightsForGroup = (group, lights) => R.pick(group.devices, lights)
