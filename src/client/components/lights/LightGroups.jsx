479f781ed3e84c086dd660da156832f0c3aae0ff
Pasi Härme <pasi.harme@gmail.com>
2018-11-11 13:57:59 +0200

    WIP: Light group controls

--- src/client/components/lights/LightGroups.jsx
+++ src/client/components/lights/LightGroups.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import { Card, Collapse, Switch, Slider } from 'antd'
+import LightbulbOnOutlineIcon from 'mdi-react/LightbulbOnOutlineIcon'
+import Brightness5Icon from 'mdi-react/Brightness5Icon'
 import * as R from 'ramda'
 
 import LightList from 'components/lights/LightList'
+
+import 'components/lights/LightGroups.css'
 
 const { Panel } = Collapse
 
@@ -12,10 +16,11 @@ const percentFormatter = (v) => `${v}%`
 const LightGroups = (props) => (
 
     R.values(props.groups).map((group, idx) =>
-        <Card key={idx} style={{ marginBottom: 8 }} bodyStyle={{ padding: 12 }}>
-            <div style={{ marginBottom: 8, color: 'rgba(0, 0, 0, 0.85)', fontWeight: 450 }}>{group.name}</div>
+        <Card key={idx} className='light-group__card'>
+            <div className='light-group__header'>{group.name}</div>
             <LightGroupControls group={group} />
-            <Collapse bordered={true} style={{ backgroundColor: 'rgba(0, 0, 0, 0)' }}>
+            <div className='light-group__divider' />
+            <Collapse bordered={false} className='light-group__collapse'>
                 <Panel header='Lights'>
                     <LightList
                         lights={lightsForGroup(group, props.lights)}
@@ -24,14 +29,38 @@ const LightGroups = (props) => (
                         updateLight={props.updateLight}
                     />
                 </Panel>
-            </Collapse>
+            </Collapse> 
         </Card>)
 )
 
-const LightGroupControls = (props) => (
-    <div>
-
-    </div>
+const LightGroupControls = ({ group }) => (
+    <table className='light-group__table'>
+        <tbody>
+            <tr>
+                <td><LightbulbOnOutlineIcon size={18} /></td>
+                <td><span>Power</span></td>
+                <td>
+                    <Switch
+                        size='small'
+                        onChange={() => null}
+                    />
+                </td>
+            </tr>
+            <tr>
+                <td><Brightness5Icon size={18} /></td>
+                <td><span>Brightness</span></td>
+                <td>
+                    <Slider
+                        min={1}
+                        max={100}
+                        onChange={() => null}
+                        onAfterChange={() => null}
+                        tipFormatter={percentFormatter}
+                    />
+                </td>
+            </tr>
+        </tbody>
+    </table>
 )
 
 const lightsForGroup = (group, lights) => R.pick(group.devices, lights)
