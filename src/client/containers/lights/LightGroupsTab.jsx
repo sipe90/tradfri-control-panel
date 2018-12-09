2d4ff8aec45e626d4b480576085e112e9db83715
Pasi Härme <pasi.harme@gmail.com>
2018-12-09 14:11:29 +0200

    Wired light groups ui components to group update actions

--- src/client/containers/lights/LightGroupsTab.jsx
+++ src/client/containers/lights/LightGroupsTab.jsx
@@ -5,7 +5,8 @@ import { Card, Switch, Slider, List } from 'antd'
 import LightbulbOnOutlineIcon from 'mdi-react/LightbulbOnOutlineIcon'
 import Brightness5Icon from 'mdi-react/Brightness5Icon'
 import * as R from 'ramda'
-import { lightStateChanged, updateLight } from 'actions/lights'
+import { lightStateChanged } from 'actions/lights'
+import { updateGroup } from 'actions/groups'
 
 import 'containers/lights/LightGroupsTab.css'
 import StatusIndicator from 'components/StatusIndicator'
@@ -13,16 +14,20 @@ import StatusIndicator from 'components/StatusIndicator'
 const Item = List.Item
 
 const percentFormatter = (v) => `${v}%`
+const anyLightIsOn = R.any(R.prop('on'))
+const lightBrightnesses = R.map(R.prop('brightness'))
+const avgBrightness = R.converge(R.divide, [R.pipe(lightBrightnesses, R.sum), R.length])
 
 class LightGroups extends Component {
 
     render() {
-        return R.values(this.props.groups).map((group, idx) =>
-            <Card key={idx} className='light-group__card' title={group.name}>
-                {this.lightGroupControls()}
+        return R.values(this.props.groups).map((group, idx) => {
+            const groupLights = lightsForGroup(group, this.props.lights)
+            return (<Card key={idx} className='light-group__card' title={group.name}>
+                {this.lightGroupControls(group, groupLights)}
                 <div className='light-group__lights-header'>Lights</div>
                 <List 
-                    dataSource={lightsForGroup(group, this.props.lights)}
+                    dataSource={groupLights}
                     renderItem={(light) => 
                         <Item>
                             <StatusIndicator type='light' alive={light.alive} on={light.on}/>{light.name}
@@ -30,9 +35,10 @@ class LightGroups extends Component {
                     bordered={true}
                     size='small' />
             </Card>)
+        })
     }
 
-    lightGroupControls() { 
+    lightGroupControls(group, groupLights) {
         return (
             <table className='light-group__table'>
                 <tbody>
@@ -41,8 +47,9 @@ class LightGroups extends Component {
                         <td><span>Power</span></td>
                         <td>
                             <Switch
+                                checked={anyLightIsOn(groupLights)}
                                 size='small'
-                                onChange={() => null}
+                                onChange={(newValue) => this.powerSwitched(group, groupLights, newValue)}
                             />
                         </td>
                     </tr>
@@ -51,9 +58,10 @@ class LightGroups extends Component {
                         <td><span>Brightness</span></td>
                         <td>
                             <Slider
+                                value={groupLights.length && avgBrightness(groupLights)}
                                 min={1}
                                 max={100}
-                                onChange={() => null}
+                                onChange={(newValue) => this.brightnessChanged(group, groupLights, newValue)}
                                 onAfterChange={() => null}
                                 tipFormatter={percentFormatter}
                             />
@@ -62,6 +70,22 @@ class LightGroups extends Component {
                 </tbody>
             </table>
         )
+    }
+
+    brightnessChanged(group, groupLights, newValue) {
+        this.props.updateGroup({ ...group, brightness: newValue})
+        for (const light of groupLights) {
+            const newLightState = { ...light, brightness: newValue }
+            this.props.lightStateChanged(newLightState)
+        }
+    }
+
+    powerSwitched(group, groupLights, newValue) {
+        this.props.updateGroup({ ...group, on: newValue})
+        for (const light of groupLights) {
+            const newLightState = { ...light, on: newValue }
+            this.props.lightStateChanged(newLightState)
+        }
     }
 }
 
@@ -72,7 +96,7 @@ LightGroups.propTypes = {
     lights: PropTypes.object.isRequired,
     initialDataLoading: PropTypes.bool.isRequired,
     lightStateChanged: PropTypes.func.isRequired,
-    updateLight: PropTypes.func.isRequired,
+    updateGroup: PropTypes.func.isRequired,
 }
 
 const mapStateToProps = state => ({
@@ -83,7 +107,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = dispatch => ({
     lightStateChanged: (light) => dispatch(lightStateChanged(light)),
-    updateLight: (gatewayId, light) => dispatch(updateLight(gatewayId, light))
+    updateGroup: (gatewayId, light) => dispatch(updateGroup(gatewayId, light))
 })
 
 export default connect(mapStateToProps, mapDispatchToProps)(LightGroups)
