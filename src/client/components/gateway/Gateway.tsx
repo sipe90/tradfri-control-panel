91c4b0ad1bb1697c9c0a18353328c5e55e32ccfa
Pasi Härme <pasi.harme@gmail.com>
2019-01-04 16:37:14 +0200

    WIP: new gateway view

--- src/client/components/gateway/Gateway.tsx
+++ src/client/components/gateway/Gateway.tsx
@@ -1,17 +1,12 @@
 import * as R from 'ramda'
 import React from 'react'
 
-import { Button, Card, Divider, Input, Popover } from 'antd'
-import { PencilIcon } from 'mdi-react'
+import { Card, Divider, Dropdown, Icon, Menu } from 'antd'
 
-import GatewayWizard from '@/components/gateway/GatewayWizard'
-import Spinner from '@/components/Spinner'
-import StatusIndicator from '@/components/StatusIndicator'
 import { GatewayConnectionState, IGateway } from 'shared/types'
 
+import StatusIndicator from '../StatusIndicator'
 import './Gateway.css'
-
-const { Meta } = Card
 
 interface IGatewayProps {
     gateway: IGateway
@@ -38,87 +33,76 @@ class Gateway extends React.Component<IGatewayProps, IGatewayState> {
         this.state = initialState
     }
 
-    public render() {
-        const { initialDataLoading, gateway } = this.props
-        return(
-            <div>
-                <Spinner spinning={initialDataLoading}>
-                    {!initialDataLoading && (!gateway ? <GatewayWizard /> : this.renderGateway())}
-                </Spinner>
-            </div>
-        )
-    }
-
-    private renderGateway = () => (
+    public render = () => (
         <div>
-            <div className='gateway__actions'>
-                <Button>Edit</Button>
-                <Button>Delete</Button>
-                <Divider type='vertical' style={{ height: 24, margin: '0px 8px 0px 4px' }} />
-                <Button>Reboot</Button>
-                <Button type='danger'>Reset</Button>
-            </div>
-            <Card
-                cover={this.cardCover()}
-            >
-                <Meta
-                    title={this.title()}
-                    avatar={this.statusIndicator()}
-                    description='IKEA Trådfri gateway'
-                />
+            <Card title={this.title()} extra={this.actionButtons()}>
+                {this.gatewayDetails()}
             </Card>
         </div>
     )
 
-    private cardCover() {
+    private title = () => (
+        <>
+            <StatusIndicator title={statusTitle(this.props.gateway)} status={status(this.props.gateway)}/>
+            {this.props.gateway.name}
+        </>
+    )
+
+    private actionButtons = () => (
+        <div className='gateway__actions'>
+            <a>Edit</a>
+            <Divider type='vertical' />
+            <a>Delete</a>
+            <Divider type='vertical' />
+            <Dropdown trigger={['click', 'hover']} overlay={this.dropdown()}>
+                <a>More <Icon type='down' /></a>
+            </Dropdown>
+        </div>
+    )
+
+    private dropdown = () => (
+        <Menu>
+          <Menu.Item>
+            <a>Reboot gateway</a>
+          </Menu.Item>
+          <Menu.Item>
+            <a>Factory reset</a>
+          </Menu.Item>
+        </Menu>
+      )
+
+    private gatewayDetails = () => {
+        const gateway = this.props.gateway
         return (
-            <div className='gateway__cover'>
-
-            </div>
+            <table className='gateway__table'>
+                <tbody>
+                    <tr>
+                        <td>
+                            Connection status
+                        </td>
+                        <td>
+                            {statusTitle(gateway)}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Hostname
+                        </td>
+                        <td>
+                            {gateway.hostname}
+                        </td>
+                    </tr>
+                    <tr>
+                        <td>
+                            Firmware version
+                        </td>
+                        <td>
+                            {gateway.version}
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
         )
-    }
-
-    private title = () => {
-        const { gateway } = this.props
-        return (
-            <div className='gateway__title'>
-                <span>{gateway.name}</span>
-                <Popover
-                    title='Edit name'
-                    trigger='click'
-                    visible={this.state.editNameVisible}
-                    onVisibleChange={this.onEditNameVisibleChanged}
-                    content={this.editName()}
-                >
-                    <span className='gateway__name-edit'>
-                        <PencilIcon size={18} />
-                    </span>
-                </Popover>
-            </div>
-        )
-    }
-
-    private statusIndicator = () => {
-        const connectionState = this.props.gateway.connectionState
-        return <StatusIndicator title={statusTitle(connectionState)} status={status(connectionState)}/>
-    }
-
-    private editName = () => {
-        return (
-            <div className='gateway__popover'>
-                <Input value={this.state.editNameText} onChange={this.editNameChanged} />
-                <Button type='primary' size='small' onClick={this.updateName}>Update</Button>
-            </div>
-        )
-    }
-
-    private onEditNameVisibleChanged = (visible: boolean) => {
-        visible && this.setState({ editNameText: this.props.gateway.name })
-        this.setState({ editNameVisible: visible })
-    }
-
-    private editNameChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
-        this.setState({ editNameText: event.target.value })
     }
 
     private updateName = () => {
@@ -130,15 +114,15 @@ class Gateway extends React.Component<IGatewayProps, IGatewayState> {
 }
 
 const statusTitle = R.cond([
-    [R.equals(GatewayConnectionState.CONNECTED), R.always('Connected to gateway')],
-    [R.equals(GatewayConnectionState.DISCONNECTED), R.always('Gateway connection lost')],
-    [R.equals(GatewayConnectionState.OFFLINE), R.always('Gateway is offline')],
+    [R.propEq('connectionState', GatewayConnectionState.CONNECTED), R.always('Connected to gateway')],
+    [R.propEq('connectionState', GatewayConnectionState.DISCONNECTED), R.always('Gateway connection lost')],
+    [R.propEq('connectionState', GatewayConnectionState.OFFLINE), R.always('Gateway is offline')],
     [R.T, R.always('Gateway is offline')]
 ])
 
 const status = R.cond([
-    [R.equals(GatewayConnectionState.CONNECTED), R.always('online')],
-    [R.equals(GatewayConnectionState.DISCONNECTED), R.always('disconnected')],
+    [R.propEq('connectionState', GatewayConnectionState.CONNECTED), R.always('online')],
+    [R.propEq('connectionState', GatewayConnectionState.DISCONNECTED), R.always('disconnected')],
     [R.T, R.always('offline')]
 ])
 
