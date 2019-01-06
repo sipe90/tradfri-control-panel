7e01415f4fb36d4e178b0df56213048ddb871a39
Pasi Härme <pasi.harme@gmail.com>
2019-01-06 14:59:39 +0200

    Update gateway changes to mock api

--- mock/server.ts
+++ mock/server.ts
@@ -1,13 +1,15 @@
 #!/usr/bin/env ts-node
 
+import 'module-alias/register'
+
 import jsonServer from 'json-server'
 const server = jsonServer.create()
 import path from 'path'
-import { IGateway, Omit } from 'shared/types'
+import { GatewayConnectionState, ICreateGatewayRequest, IGateway, IUpdateGatewayRequest } from 'shared/types'
 const router = jsonServer.router(path.join(__dirname, 'db.json'))
 const middlewares = jsonServer.defaults()
 
-let gateway: Omit<IGateway, 'connected'>
+let gateway: IGateway | null
 
 server.use(middlewares)
 server.use(jsonServer.bodyParser)
@@ -17,10 +19,7 @@ server.use(jsonServer.rewriter({
 }))
 
 server.get('/gateway', (_req, res) =>
-    gateway ? res.json({
-        ...gateway,
-        connected: true
-    }) : res.status(404).send()
+    gateway ? res.json(gateway) : res.sendStatus(404)
 )
 
 server.get('/gateway/discover', (_req, res) =>
@@ -50,9 +49,55 @@ server.post('/gateway/test', (_req, res) =>
 )
 
 server.post('/gateway', (req, res) => {
-    gateway = req.body
-    res.status(201).send()
+    gateway = {
+        ...req.body as ICreateGatewayRequest,
+        connectionState: GatewayConnectionState.CONNECTED,
+        alexaPairStatus: false,
+        googleHomePairStatus: false,
+        version: '1.4.15',
+        updateProgress: 0,
+        updatePriority: 5,
+        releaseNotes: ''
+    }
+    res.sendStatus(201)
 })
+
+server.post('/gateway/update', (req, res) => {
+    if (!gateway) return res.sendStatus(404)
+    if (gateway.hostname !== req.body.name) {
+        simulateConnectionLoss(5000, 20000)
+    }
+    gateway = {
+        ...gateway,
+        ...req.body as IUpdateGatewayRequest,
+    }
+    res.sendStatus(204)
+})
+
+server.post('/gateway/reboot', (_req, res) => {
+    if (!gateway) return res.sendStatus(404)
+    simulateConnectionLoss(5000, 20000)
+    res.status(204).send()
+})
+
+server.post('/gateway/reset', (_req, res) => {
+    if (!gateway) return res.sendStatus(404)
+    gateway = null
+    res.sendStatus(204)
+})
+
+const simulateConnectionLoss = (startAfter: number, recoverAfter: number) => {
+    setTimeout(() => {
+        if (gateway) {
+            gateway.connectionState = GatewayConnectionState.OFFLINE
+            setTimeout(() => {
+                if (gateway) {
+                    gateway.connectionState = GatewayConnectionState.CONNECTED
+                }
+            }, recoverAfter)
+        }
+    }, startAfter)
+}
 
 server.use(router)
 
