3896cfae0f98d2dae003eb4b7cad60aaf2993b88
Pasi Härme <pasi.harme@gmail.com>
2019-03-03 21:08:17 +0200

    WIP: websocket support

--- src/server/init.ts
+++ src/server/init.ts
@@ -1,44 +1,45 @@
+import WebSocket from 'ws'
+
 import * as db from '#/db'
 import logger from '#/logger'
 import { setupCircadian } from '#/service/circadian-service'
-import { connectToGateway } from '#/service/gateway-connection-manager'
+import * as manager from '#/service/gateway-connection-manager'
 import { fetchGateway } from '#/service/gateway-service'
+import WebSocketBroker from './component/WebSocketBroker'
 
 const logError = (logMsg: string) => (err: Error) => {
     logger.error(logMsg, err)
     return Promise.reject(err)
 }
 
-export default async (env: string) => {
-    try {
-        logger.info('Initializing application')
+export default async (wss: WebSocket.Server, env: string) => {
+    logger.info('Initializing application')
 
-        await db.init(env).catch(logError('Failed to connect to LokiJS database.'))
+    await db.init(env).catch(logError('Failed to connect to LokiJS database.'))
 
-        logger.info('Successfully connected to LokiJS database')
+    logger.info('Successfully connected to LokiJS database')
 
-        logger.info('Fetching gateway from database')
-        const gateway = await fetchGateway()
+    logger.info('Fetching gateway from database')
+    const gateway = await fetchGateway()
 
-        if (!gateway) {
-            logger.info('No gateway found from database')
-            return true
-        }
-
-        logger.info('Found gateway from database')
-
-        await connectToGateway(gateway).catch(logError('Failed to connect to Gateway'))
-
-        logger.info('Finished connecting to gateway')
-
-        logger.info('Setting up circadian')
-
-        await setupCircadian()
-
-        return true
-    } catch (err) {
-        logger.error('Initialization failed')
-        logger.error(err)
-        return false
+    if (!gateway) {
+        logger.info('No gateway found from database')
+        return
     }
+
+    logger.info('Found gateway from database')
+
+    const { hostname, identity, psk } = gateway
+    await manager.connectToGateway(hostname, identity, psk).catch(logError('Failed to connect to Gateway'))
+    manager.setBroker(new WebSocketBroker(wss))
+
+    logger.info('Setting up gateway observers')
+
+    manager.observeGateway()
+
+    logger.info('Finished connecting to gateway')
+
+    logger.info('Setting up circadian')
+
+    await setupCircadian()
 }
--- src/server/service/gateway-connection-manager.ts
+++ src/server/service/gateway-connection-manager.ts
@@ -1,24 +1,31 @@
+import WebSocketBroker from '#/component/WebSocketBroker'
 import TradfriGateway from '#/gateway/TradfriGateway'
 import logger from '#/logger'
+import { Accessory, AllEventCallbacks, GatewayDetails, Group, Scene } from 'node-tradfri-client'
+import { Entities, PayloadTypes } from 'shared/types'
 
-interface IConnectProps {
-    hostname: string
-    identity: string
-    psk: string
+let _connection: TradfriGateway | null = null
+let _broker: WebSocketBroker | null = null
+
+export const setBroker = (webSocketBroker: WebSocketBroker) => _broker = webSocketBroker
+
+export const getBroker = () => {
+    if (!_broker) {
+        throw Error('Cannot get broker: Not set')
+    }
+    return _broker
 }
-
-let connection: TradfriGateway | null = null
 
 export const getConnection = () => {
-    if (!connection) {
+    if (!_connection) {
         throw Error('Cannot get connection: Not connected to gateway')
     }
-    return connection
+    return _connection
 }
 
-export const isGatewayConnected = () => connection ? connection.isConnected() : false
+export const isGatewayConnected = () => _connection ? _connection.isConnected() : false
 
-export const connectToGateway = async ({ hostname, identity, psk }: IConnectProps) => {
+export const connectToGateway = async (hostname: string, identity: string, psk: string) => {
     logger.info(`Connecting to a trådfri gateway at hostname ${hostname}`)
     const gateway = new TradfriGateway(hostname, true)
 
@@ -30,21 +37,43 @@ export const connectToGateway = async ({ hostname, identity, psk }: IConnectProp
         logger.error(err)
     }
 
-    connection = gateway
-    return connection
+    _connection = gateway
+    return _connection
 }
+
+export const observeGateway = () => {
+    const connection = getConnection()
+    const broker = getBroker()
+
+    const sendData = dataSender(broker)
+
+    const observers: Partial<AllEventCallbacks> = {
+        'gateway updated': (gateway: GatewayDetails) => sendData('update', 'gateway', gateway.clone()),
+        'group updated': (group: Group) => sendData('update', 'group', group.clone()),
+        'group removed': (groupId: number) => sendData('remove', 'group', groupId),
+        'device updated': (device: Accessory) => sendData('update', 'device', device.clone()),
+        'device removed': (deviceId: number) => sendData('remove', 'device', deviceId),
+        'scene updated': (groupId: number, scene: Scene) => sendData('update', 'scene', { ...scene.clone(), groupId }),
+        'scene removed': (groupId: number, sceneId: number) => sendData('remove', 'scene', { groupId, sceneId }),
+    }
+
+    connection.registerObservers(observers)
+}
+
+const dataSender = (broker: WebSocketBroker) => (type: PayloadTypes, entity: Entities, data: any) =>
+    broker.broadcast({ type, entity, data })
 
 export const disconnectFromGateway = (clear: boolean) => {
     logger.info('Disconnecting from gateway [clear=%b]', clear)
-    if (!connection && !clear) {
+    if (!_connection && !clear) {
         throw Error('Cannot disconnect: Not connected to gateway')
     }
-    if (connection) {
-        connection.disconnect()
+    if (_connection) {
+        _connection.disconnect()
         logger.info('Successfully disconnected from gateway')
     }
     if (clear) {
         logger.info('Clearing gateway connection')
-        connection = null
+        _connection = null
     }
 }
